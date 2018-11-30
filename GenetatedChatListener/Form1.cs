4030627c866f5179af8c8f0d881a69c49d788e7c
Igor Kazantsev <43610577+NexoKazan@users.noreply.github.com>
2018-11-30 10:04:57 +0300

    1

--- GenetatedChatListener/Form1.cs
+++ GenetatedChatListener/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4;
+using Antlr4.Runtime.Tree;
 
 namespace GenetatedChatListener
 {
     public partial class Form1 : Form
     {
+        string output;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputString = textBox1.Text;
+            ICharStream inputStream = new AntlrInputStream(inputString.ToString());
+            ITokenSource speakLexer = new SpeakLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(speakLexer);
+            SpeakParser speakParser = new SpeakParser(commonTokenStream);
+            speakParser.BuildParseTree = true;
+            IParseTree tree = speakParser.chat();
+            MyListener l = new MyListener();
+            speakParser.line();        
+            output = l.line;
+            textBox1.Text = output;
         }
     }
 }
---
+++ GenetatedChatListener/MyListener.cs
@@ -0,0 +1,25 @@
+﻿using Antlr4.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenetatedChatListener
+{
+    class MyListener : SpeakBaseListener
+    {
+        public string line;
+        public override void EnterChat([NotNull] SpeakParser.ChatContext context) {
+            line += "EnterChat: " + context.line().ToString() + "\r\n";
+        }
+        public override void ExitChat([NotNull] SpeakParser.ChatContext context) {
+            line += "ExitChat: " + context.line().ToString() + "\r\n";
+        }
+        //public virtual void EnterLine([NotNull] SpeakParser.LineContext context) { }
+        //public virtual void ExitLine([NotNull] SpeakParser.LineContext context) { }
+        public override void EnterName([NotNull] SpeakParser.NameContext context) {
+           // line += "EnterName: " + context.WORD().GetText() + "\r\n";
+        }
+    }
+}
